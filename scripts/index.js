4e6a715f8183b82eaf1f4ecb8e71e0a1f376e501
doralex <76019292+doralex@users.noreply.github.com>
2021-02-28 00:40:03 +0300

    Исправления согласно ревью

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let editForm = document.querySelector('.edit-form');
+let closeButton = editForm.querySelector('.edit-form__close');
+let nameInput = editForm.querySelector('.edit-form__item-form_title');
+let aboutInput = editForm.querySelector('.edit-form__item-form_subtitle');
+let profile = document.querySelector('.profile')
+let editButton = profile.querySelector('.profile__edit-button');
+let profileTitle = profile.querySelector('.profile__title');
+let profileSubTitle = profile.querySelector('.profile__subtitle');
+
+function togglePopup() {
+    editForm.classList.toggle('edit-form_opened');
+    if (editForm.classList.contains('edit-form_opened')) {
+        nameInput.value = profileTitle.textContent;
+        aboutInput.value = profileSubTitle.textContent;
+    }
+}
+
+function formSubmit (event) {
+    event.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubTitle.textContent = aboutInput.value;
+    togglePopup();    
+}
+
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+editForm.addEventListener('submit', formSubmit);
